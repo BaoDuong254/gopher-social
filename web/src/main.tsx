2d0be51e15c6d3fdce910b49d4950ef7edc2f9c0
GiaBao <duonggiabao254@gmail.com>
2026-04-03 16:51:11 +0700

    feat: add user confirmation, login, post creation, and single post view functionality
    - Implemented ConfirmationPage for user account activation.
    - Created CreatePostForm for submitting new posts.
    - Developed LoginPage for user authentication and token management.
    - Added Post component to display individual posts with comments.
    - Implemented SinglePost component to view detailed post information and comments.
    - Added vite-env.d.ts for TypeScript support in Vite.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,23 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ConfirmPage from "./page/ConfirmPage.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ConfirmationPage } from './ConfirmationPage.tsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App />
   },
   {
     path: "/confirm/:token",
-    element: <ConfirmPage />,
+    element: <ConfirmationPage />
   },
-]);
+])
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-);
+)
